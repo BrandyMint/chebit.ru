58a66253b2ba1370a4653a5c5ed0f5401f18e42a
Danil Pismenny <danil@orionet.ru>
2011-01-29 22:04:58 +0300

    deploy

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible( :email, :password, :password_confirmation, :remember_me,
-                   :tel, :login, :firm
+                   :tel, :login, :firm,
                    :full_name, :occupation, :website, :note,
                    :discourse_state, :discourse_subject, :price_confirm, :tag_list )
   

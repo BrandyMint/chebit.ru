a3a50d65103867bed621fed198a141646e3a902a
Danil Pismenny <danil@orionet.ru>
2011-01-30 13:03:46 +0300

    deploy

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,10 @@
 class WelcomeController < ApplicationController
   def index
     @users = User.all
+    @tags = User.tag_counts
+
+    # %w=[Ruby PHP Jave Servlet ASP.NET Perl Flash Flex MySQL postgresql JavaScript AJAX HTML5 xHTML CSS Apache ngnix Linux TDD/BDD rspec cucumber]
+
   end
 
 end

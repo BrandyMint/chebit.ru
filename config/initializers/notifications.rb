ff54d2425eee84ee61a2316aecd09921333e18e0
Danil Pismenny <danil@orionet.ru>
2011-02-02 21:56:45 +0300

    deploy

--- config/initializers/notifications.rb
+++ config/initializers/notifications.rb
@@ -1,14 +1,5 @@
 Chebit::Application.config.middleware.use( ExceptionNotifier,
-                                           # :email_prefix => "[Whatever] ",
+                                           :email_prefix => "[CHEBiT] ",
                                            :sender_address => Settings.mail.sender,
                                            :exception_recipients => Settings.mail.admin_email
                                            )
-#end
-
-
-# ebytoday::Application.configure do
-#   config.middleware.use "::ExceptionNotifier",
-#   :email_prefix => "[WebApp Error] ",
-#   :sender_address => MAIL_CONFIG[:sender], # %{"notifier" <notifier@chebytoday.ru>},
-#   :exception_recipients => MAIL_CONFIG[:recipient] #%w{danil@orionet.ru} 
-# end

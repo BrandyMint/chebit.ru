6510af1293cfb117a383cbc474e66b744299ab85
alekspav <pavlovnew@mail.ru>
2011-07-02 09:02:20 -0400

    Добавлен javascript при наведении на комментарии, еще в предыдущем коммите

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ActiveRecord::Base
   attr_accessible :content, :commentable_id, :commentable_type
   belongs_to :commentable, :polymorphic => true
   belongs_to :author, :class_name => "User"
+  has_many :comments, :as => :commentable
 end

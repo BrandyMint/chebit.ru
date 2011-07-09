18d2e1fd550f28f58a17f48c2ecbaec2606ed69b
alekspav <pavlovnew@mail.ru>
2011-07-09 06:05:33 -0400

    Добавлены валидации и колбэки

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,7 @@
 class Comment < ActiveRecord::Base
-  attr_accessible :content, :commentable_id, :commentable_type, :author
+  attr_accessible :content, :commentable_id, :commentable_type, :author 
+  validates_presence_of :commentable, :content
+
   belongs_to :commentable, :polymorphic => true
   belongs_to :author, :class_name => "User"
   has_many :comments, :as => :commentable

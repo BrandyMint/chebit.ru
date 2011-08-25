c4d5e3508e1b24c3075a00483dafd173037d09ca
Danil Pismenny <danil@orionet.ru>
2011-08-25 11:25:34 +0400

    логотип бедункевича (smartmedia)

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,19 +1,40 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+// // Яваскрипт для подсветки точек перед комментариями
+// sender_comment = '';
+// function showdiv(sender,classname){
+//   sender.getElementsByClassName(classname)[0].style.display = "inline";
+//   sender_comment = sender;
+// }
 
+// //Скрывает div
+// function hidediv(sender,classname){
+//   if(sender != ''){
+//     sender.getElementsByClassName(classname)[0].style.display = "none";
+//   }
+// }
 
-// Яваскрипт для подсветки точек перед комментариями
-sender_comment = '';
-function showdiv(sender,classname){
-  sender.getElementsByClassName(classname)[0].style.display = "inline";
-  sender_comment = sender;
-}
-//Скрывает div
-function hidediv(sender,classname){
-if(sender != ''){
-  sender.getElementsByClassName(classname)[0].style.display = "none";
- }
-}
+$(function() {
+    
+    $('.j-comment-answer').
+      click(function(event){
+              t = $(this);
+              commentable = t.data('commentable');
+              form = $("#new_comment_form_" + commentable)
+              form.toggle();
+              if (form.is(':visible')) {
+                t.text(t.data('visible-text') || 'скрыть');
+              } else {
+                t.text(t.data('hidden-text') || 'ответить');
+              }
+              return false;
+        });
+  });
 
-
-
+// $(document).ready(function(){
+//                     //JS для реализации комментариев в проекте
+//                     $('.j-comment-answer').click(function(event){
+//                                                    showdiv(this.parentNode,'add-comment')
+//                                                    event.preventDefault();
+//                                                  });
+//                   });

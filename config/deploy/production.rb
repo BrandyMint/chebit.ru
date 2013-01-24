beb9904279bea65535fcd47925a911bdaf84ec39
Alexey Illarionov <littlesavage@rambler.ru>
2013-01-24 10:58:58 +0400

    Забытые файлы

---
+++ config/deploy/production.rb
@@ -0,0 +1,7 @@
+#Конфиг деплоя на production
+server 'brandymint.ru', :app, :web, :db, :primary => true
+set :branch, "master" unless exists?(:branch)
+set :rails_env, "production"
+
+require 'airbrake/capistrano'
+
